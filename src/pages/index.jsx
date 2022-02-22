9d99a322ac7e2ee6002706c60735c6523868c9d5
Lucas <lucashenriquebranco1@hotmail.com>
2022-02-21 22:45:37 -0300

    componente tabela

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,24 @@
 import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+import Cliente from "../core/Cliente"
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Lucas', 20, '1'),
+    new Cliente('Jane', 60, '2'),
+    new Cliente('Mari', 18, '3'),
+    new Cliente('Paula', 35, '4'),
+  ]
+
+  function clienteSelecionado(cliente, Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente, Cliente) {
+    
+  }
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -8,7 +26,9 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}></Tabela>
       </Layout>
     </div>
   )
